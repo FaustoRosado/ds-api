fdd598fca6f95e442770291c9fa3fddae9b53cb0
FaustoRosado <faustrosado@gmail.com>
2020-12-17 15:21:21 -0500

    added students

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
 from flask import Flask, request, jsonify
+import requests
+import json
+
+students = {"s1":"fausto",
+            "s2":"anna", 
+            "s3":"rakshanda",
+            "s4":"elston", 
+            }
+
+
+headers = {"Content-Type": "application/json"}
+
+r = requests.post("http://127.0.0.1:5000/", params=students, headers=headers)
+json_resp = json.loads(r.text) 
+json_resp
+
+
+
+headers = {"Content-Type": "application/json"}
+
+r = requests.delete("http://127.0.0.1:5000/", params=student, headers=headers)
+json_resp = json.loads(r.text) 
+json_resp
 
 app = Flask(__name__)
 
 d = {}
 
 @app.route('/', methods=['GET'])
-def get_records():
+def get_student():
     return jsonify(d)
 
 @app.route('/', methods=['POST'])
-def create_record():
+def create_student():
     added = {}
     for k,v in request.args.items():
         if not k in d.keys():
@@ -18,7 +41,7 @@ def create_record():
     return jsonify({"added": added, "current": d})
 
 @app.route('/', methods=['DELETE'])
-def delete_record():
+def delete_student():
     deleted = {}
     for k,v in request.args.items():
         try:
